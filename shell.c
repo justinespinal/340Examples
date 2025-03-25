7e9d0986f4122bebb9e0bebb8c656517c8a7fe41
justinespinal <espinaljustin03@gmail.com>
2025-03-24 22:46:14 -0400

    redirection

--- shell.c
+++ shell.c
@@ -3,6 +3,8 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <fcntl.h>
 
 void tokenize(char input[], char *args[]){
     int i = 0;
@@ -14,10 +16,57 @@ void tokenize(char input[], char *args[]){
     args[i] = NULL;
 }
 
-void addParams( char *args[], char *params[], int start, int end){
-    for( int i = start; i <= end; i++ ){
-        params[i] = args[i];
+void handle_redirect(int left, char leftSide[], char rightSide[]){
+    if(strstr(leftSide, "<") != NULL || strstr(leftSide, ">") != NULL){
+        // leftSide = handle_redirect();
     }
+    if(strstr(rightSide, "<") != NULL || strstr(rightSide, ">") != NULL){
+        // rightSide = handle_redirect();
+    }
+
+    if(left){
+        pid_t pid;
+        if((pid = fork()) == 0){
+            int fd = open(rightSide, O_RDONLY);
+
+            if (fd < 0) {
+                perror("open failed");
+                exit(1);
+            }
+
+            dup2(fd, 0);
+            close(fd);
+            char *args[10];
+            tokenize(leftSide, args);
+            execvp(args[0], args);
+            perror("exec failed");
+            exit(1);
+        }
+        waitpid(pid, NULL, 0);
+    }else{
+        pid_t pid;
+        if((pid = fork()) == 0){
+            int fd = open(rightSide, O_WRONLY);
+
+            if (fd < 0) {
+                perror("open failed");
+                exit(1);
+            }
+
+            dup2(fd, 1);
+            close(fd);
+            char *args[10];
+            tokenize(leftSide, args);
+            execvp(args[0], args);
+            perror("exec failed");
+            exit(1);
+        }
+        waitpid(pid, NULL, 0);
+    }
+}
+
+void execute_pipeline(){
+
 }
 
 int main(){
@@ -29,22 +78,59 @@ int main(){
 
     while(1){
         printf("> ");
-        commands[100];
         fgets(commands, sizeof(commands), stdin);
-        if(strcmp(commands, "exit\n") == 0){
+        commands[strcspn(commands, "\n")] = 0;
+
+        if(strcmp(commands, "exit") == 0){
             exit(0);
         }
-        commands[strcspn(commands, "\n")] = 0;
+
+        if (strchr(commands, '<') != NULL) {
+            char *leftSide = strtok(commands, "<");
+            char *rightSide = strtok(NULL, "<");
+
+            leftSide[strcspn(leftSide, "\n")] = 0;
+            rightSide[strcspn(rightSide, "\n")] = 0;
+            while (*rightSide == ' ') rightSide++;
+
+            handle_redirect(1, leftSide, rightSide);
+            continue;
+        }
+
+        if (strchr(commands, '>') != NULL) {
+            char *leftSide = strtok(commands, ">");
+            char *rightSide = strtok(NULL, ">");
+
+            leftSide[strcspn(leftSide, "\n")] = 0;
+            rightSide[strcspn(rightSide, "\n")] = 0;
+            while (*rightSide == ' ') rightSide++;
+
+            handle_redirect(0, leftSide, rightSide);
+            continue;
+        }
+
         tokenize(commands, args);
 
         if(strcmp(args[0], "cd") == 0){
-            chdir(args[1]);
+            if (chdir(args[1]) != 0) {
+                perror("cd failed");
+            }
+            continue;
+        }
+
+        if(strcmp(args[0], "time") == 0){
+            time_t t;
+            time(&t);
+            char *current_time = ctime(&t);
+            printf("%s", current_time);
             continue;
         }
 
         pid_t pid;
-        if((pid = fork()) == 0){
+        if ((pid = fork()) == 0) {
             execvp(args[0], args);
+            perror("execution failed");
+            exit(1); // exit the child if exec fails
         }
         waitpid(pid, NULL, 0);
     }
